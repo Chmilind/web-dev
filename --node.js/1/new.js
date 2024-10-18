9253639b01c8078dc70459f790cb5b777bc9dd78
Milind <milind2bhilai@gmail.com>
2024-10-18 18:17:13 +0530

    ji

---
+++ --node.js/1/new.js
@@ -0,0 +1,4 @@
+console.log("Hello world")
+console.log("You are good")
+console.log("You are fine")
+console.log("You are great")
---
+++ javascript@/1/script.js
@@ -0,0 +1,22 @@
+alert("Hello world !!");
+
+console.log("Hey I am console")
+console.log("Hey I am console god you know")
+console.log("Hey I am like it ")
+
+var a = prompt("Enter your number")
+var isTrue = prompt("Are you sure")
+
+if(isTrue){
+    console.log("Computer is blasting")
+}
+
+else{
+    console.log("Computer is not sblasting")
+
+}
+console.log("your number is " + a)
+
+document.title = "i am good"
+
+// document.body.style.backgroundColor = "red"
