12ccfeea9507515457d928f2e1a1674f2a968e88
Milind <milind2bhilai@gmail.com>
2024-10-21 20:10:33 +0530

    GG

---
+++ javascript@/2.VARIABLES , DATA TYPE & OBJECT/script.js
@@ -0,0 +1,63 @@
+console.log("VARIABLE , DATA TYPE , OBJECT")
+
+// var a = 5;'
+// a = a + 1
+// var b = 6;
+// var c= "milind";
+// var 55a = "rohan"; not allowed
+
+
+// console.log(a+b)
+// console.log(a+b+5)
+
+// console.log(typeof a,typeof b,typeof c)
+
+// const a1 = 6;
+// a1 = a1 + 1; // not allowed -- const cant be change any where
+
+
+let a = 5;
+let b = 6;
+let c= "milind";
+
+console.log(a+b)
+console.log(a+b+5)
+console.log(typeof a,typeof b,typeof c)
+
+{
+    let a = 66;
+    console.log(a)
+}
+
+console.log(a)
+
+
+// primitive data type 
+
+let x = "Harry";
+let y = 22;
+let z = 3.33;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r);
+
+
+// object
+
+let o = {
+    "name": "harry",
+    "job code": 5600,
+    "is_hundsome":true
+}
+
+console.log(o)
+
+o.salary = "10lakhs"
+console.log(o)
+o.salary = "50lakhs"
+console.log(o)
+
+
