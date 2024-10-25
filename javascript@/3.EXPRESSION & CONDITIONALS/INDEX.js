e1ebdad142653b18c37bc911f1c96feab1011f8c
Milind <milind2bhilai@gmail.com>
2024-10-25 13:52:29 +0530

    sdcsc

---
+++ javascript@/3.EXPRESSION & CONDITIONALS/INDEX.js
@@ -0,0 +1,54 @@
+console.log("Hello I am conditional tuturiol")
+
+// let age = 1;
+// let grace = 2;
+
+// age += grace  //assignment operator 
+
+// console.log(age+grace);  //arithmetic operator   
+// console.log(age-grace);  //arithmetic operator  
+// console.log(age*grace);  //arithmetic operator  
+// console.log(age/grace);  //arithmetic operator  
+// console.log(age**grace);  //arithmetic operator  
+// console.log(age%grace);  //arithmetic operator  
+
+// if((age+grace)>18){
+
+let age = 1;
+
+if(age==18){
+    console.log(" You can drive ")
+}
+
+else if(age==0){
+    console.log("Are you kidding me")
+}
+
+else if(age==1){ 
+    console.log(" Are you kidding me again ")
+}
+
+else{
+    console.log("you cannot drive")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
