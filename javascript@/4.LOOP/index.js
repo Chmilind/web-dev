69a60630407cbd91053811b1d502f563fe486ead
Milind <milind2bhilai@gmail.com>
2024-10-27 21:21:45 +0530

    loop

---
+++ javascript@/4.LOOP/index.js
@@ -0,0 +1,48 @@
+console.log("----About loops");
+
+let a = 1;
+
+// console.log("1")
+// console.log("2")
+// console.log("3")
+
+// for loop 
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(a+i);
+// }               
+
+// let obj ={
+//     name: "John",
+//     age: 30,
+//     city: "New York",
+//     occupation: "Software Engineer",
+// }
+
+// forin loop
+
+// for (const key in obj) {
+//         console.log(key);
+// }
+
+// forof loop
+
+// for (const c of "New York") {
+//     console.log(c);
+// }
+
+// while loop 
+
+// let i = 0;
+// while (i<6) {
+//     console.log(i);
+//     i++;
+// }
+
+// do-while loop 
+
+let i = 90;
+do {
+    console.log(i);
+    i++;
+} while (i<8);
