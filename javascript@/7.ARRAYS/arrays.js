bee8853e10e456e37175382131568698149d1cc9
Milind <milind2bhilai@gmail.com>
2024-10-30 00:32:22 +0530

    cc

---
+++ javascript@/7.ARRAYS/arrays.js
@@ -0,0 +1,77 @@
+console.log("About arrays and its basic logic")
+console.log("-----------")
+console.log("Arrays are a collection of items stored in a single variable.")
+console.log("-----------")
+
+let arr = [1, 2, 3, 4, 5, 6]
+//index -- 0  1  2  3  4  5
+
+console.log(arr)  // Output: [1, 2, 3, 4, 5, 6]
+console.log(arr.length) // 6
+console.log(arr[3])
+     // arrays are mutabl
+console.log(arr[3]=20) //or arr[3] = 4242; 
+console.log(arr)   //  [1, 2, 3, 20, 5, 6]
+
+// Array methods
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
