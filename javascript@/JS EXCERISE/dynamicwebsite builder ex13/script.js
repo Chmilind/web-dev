f706dbed91bb46ee62d04b678f3e84a2cf9c6889
Milind <milind2bhilai@gmail.com>
2024-12-26 23:16:27 +0545

    ex 13

---
+++ javascript@/JS EXCERISE/dynamicwebsite builder ex13/script.js
@@ -0,0 +1,10 @@
+// function createCard(title,cName,views, monthsOld,duration,thumbnail){
+//     //finish the function 
+// }
+
+// createCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithHarry", 560000, 7, "31:22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw")
+
+
+function createCard(title, cName , views, monthsOld, duration, thumbnail){
+  let html = ``
+}
